3e4e432b0ae182b5ecb731f47f98288577fac205
Valentin Degenne <vdegenne@gmail.com>
2019-05-20 16:47:29 +0200

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-appname=$(awk 'NR==1 {print substr($0,1,length($0)-1)}' .server)
+appname=$(awk 'NR==1' .server)
 port=$(awk 'NR==2' .server)
 
 pm2 start node --name "$appname:$port" --watch -- --experimental-modules serve.mjs -p $port
